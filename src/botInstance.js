795d04461c1b3814a52da300ea3e1d80f61e24c7
Victor Nosov <scaramou4@gmail.com>
2025-01-31 11:49:59 +0300

    commands to botInstance added

--- src/botInstance.js
+++ src/botInstance.js
@@ -4,4 +4,10 @@ require('dotenv').config();
 // Создаём бота
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 
+// Задаём команды бота
+bot.setMyCommands([
+  { command: '/start', description: 'Запуск бота' },
+  { command: '/list', description: 'Показать активные напоминания' },
+]);
+
 module.exports = bot;
